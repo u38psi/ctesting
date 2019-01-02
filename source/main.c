12fe6196ea5fcebb3e7facf450e541c63ac960e7
u38psi <u38psi@tutanota.com>
2019-01-02 23:54:00 +0100

    updates to c main

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include "SDL/SDL_image.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
 
 
 typedef struct Rect
@@ -15,16 +16,51 @@ typedef struct Rect
 	int height;
 	int xpos;
 	int ypos;
+	int xvel;
+	int yvel;
 	SDL_Rect rect;
 	
 } Rect;
+
+void clearBlack(SDL_Renderer *rend)
+{
+	SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
+	SDL_RenderClear(rend);
+}
+
+void drawRect(SDL_Renderer *rend, Rect rect)
+{
+
+	SDL_SetRenderDrawColor(rend, rect.red, rect.green, rect.blue, rect.alpha);
+	
+	rect.rect.x = rect.xpos;
+	rect.rect.y = rect.ypos;
+	rect.rect.w = rect.width;
+	rect.rect.h = rect.height;
+
+	SDL_RenderFillRect(rend, &rect.rect);
+	SDL_RenderPresent(rend);
+
+}
 
 int main(int argc, char *argv[])
 {
 	int game = 1;
 
+	srand(time(NULL));
+
+	int initX = rand() % 2;
+	int initY = rand() % 2;
+
+	int frameCount;
+
+	int winWidth = 750;
+	int winHeight = 600;
+
 	SDL_Window *win;
 	SDL_Renderer *rend; 
+
+	SDL_DisplayMode dm;
 
 	SDL_Event event;
 	
@@ -38,8 +74,8 @@ int main(int argc, char *argv[])
 			"Testing", 
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
-			750,
-			600,
+			winWidth,
+			winHeight,
 			SDL_WINDOW_OPENGL);
 
 	if (!win)
@@ -48,33 +84,53 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	if (SDL_GetCurrentDisplayMode(0, &dm) != 0)
+	{
+		SDL_Log("%s\n", SDL_GetError());
+		return 1;
+	}
+
 	rend = SDL_CreateRenderer(win, -1, 0);
 
-	SDL_SetRenderDrawColor(rend, 0, 100, 200, 255);
-	SDL_RenderClear(rend);
+	clearBlack(rend);
 	
-	Rect rect = {255, 255, 255, 255, 
-			200, 100, 500, 500};
+	Rect player1 = {255, 255, 255, 255, 
+			10, 100, 0, winHeight/2, 0, 0};
+
+	Rect player2 = {255, 255, 255, 255, 
+			10, 100, winWidth-10, winHeight/2, 0, 0};
+
+	Rect ball = {255, 255, 255, 255,
+		     10, 10, winWidth/2-ball.width/2, rand() % (winHeight-ball.height), 0, 0};
 	
-	SDL_SetRenderDrawColor(rend, rect.red, rect.green, rect.blue, rect.alpha);
+	if (initX)
+	{
+		ball.xvel = 5;
+	} else 
+	{
+		ball.xvel = -5;
+	}
+
+	if (initY)
+	{
+		ball.yvel = 5;
+	} else 
+	{
+		ball.yvel = -5;
+	}
 
 	while (game)
 	{
+		clearBlack(rend);
+
+		drawRect(rend, player1);
+		drawRect(rend, player2);
+		drawRect(rend, ball);
 		
+			
 		while (SDL_PollEvent(&event))
 		{	
 
-					
-			SDL_SetRenderDrawColor(rend, rect.red, rect.green, rect.blue, rect.alpha);
-
-			rect.rect.w = rect.width;	
-			rect.rect.h = rect.height;
-			rect.rect.x = rect.xpos;
-			rect.rect.y = rect.ypos;
-
-			SDL_RenderFillRect(rend, &rect.rect);
-			SDL_RenderPresent(rend);
-				
 			if (SDL_QuitRequested())
 				game = 0;
 			
@@ -83,53 +139,110 @@ int main(int argc, char *argv[])
 				case SDL_KEYDOWN:
 					switch (event.key.keysym.sym)
 					{
-						case 0x0D:
-							game = 0;
+
+						case SDLK_w:
+							player1.yvel = -30;
+							break;
+
+						case SDLK_s:										
+							player1.yvel = 30;	
+							break;
 						
-						case 0x61:
-							rect.xpos -= 5;
-							SDL_Log("%d\n", rect.xpos);
-							SDL_SetRenderDrawColor(rend, 
-										0, 100, 
-										200, 255);
-							SDL_RenderClear(rend);
-							break;
-						case 0x64:
-							rect.xpos += 5;
-							SDL_Log("XPOS: %d\n", rect.xpos);
-							SDL_SetRenderDrawColor(rend, 
-										0, 100, 
-										200, 255); 
-							SDL_RenderClear(rend);
+						case SDLK_UP:													
+							player2.yvel = -30;
 							break;
 
-						case 0x77:
-							rect.ypos -= 5;
-							SDL_Log("YPOS: %d\n", rect.ypos);
-							SDL_SetRenderDrawColor(rend,
-										0, 100,
-										200, 255);
-							SDL_RenderClear(rend);
+						case SDLK_DOWN:	
+							player2.yvel = 30;
 							break;
 
-						case 0x73:
-							rect.ypos += 5;
-							SDL_Log("YPOS: %d\n", rect.ypos);
-							SDL_SetRenderDrawColor(rend,
-										0, 100,
-										200, 255);
-							SDL_RenderClear(rend);
-							break;
 						default: 
 							break;
-					}				
+					}
+					
+					break;
+
+				case SDL_KEYUP:
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_w:
+							if (player1.yvel < 0)
+								player1.yvel = 0;
+							break;
+
+						case SDLK_s:
+							if (player1.yvel > 0)
+								player1.yvel = 0;
+							break;
+						
+						case SDLK_UP:
+							if (player2.yvel < 0)
+								player2.yvel = 0;
+							break;
+						
+						case SDLK_DOWN:
+							if (player2.yvel > 0)
+								player2.yvel = 0;
+							break;
+						default:
+							break;
+					}
+					
+					break;				
 			
 					
 				default:
 					break;
 			}
-		}
+			
+			player1.ypos += player1.yvel;
+			player2.ypos += player2.yvel;
+
+
+			if (player1.ypos <= 0)
+			{
+				player1.ypos = 0;
+			} else if (player1.ypos >= (winHeight-player1.height))
+			{
+				player1.ypos = winHeight-player1.height;
+			} 
+
+			if (player2.ypos <= 0)
+			{
+				player2.ypos = 0;
+			} else if (player2.ypos >= (winHeight-player2.height))
+			{
+				player2.ypos = winHeight-player2.height;
+			}
 	
+		}
+
+		if (ball.ypos <= 0)
+		{
+			ball.yvel = 5;
+		} else if (ball.ypos >= winHeight-ball.height)
+		{
+			ball.yvel = -5;
+		}
+
+		if (ball.xpos <= player1.xpos + player1.width && ball.ypos <= player1.ypos + player1.height && ball.ypos >= player1.ypos)
+		{
+			ball.xvel = 5;
+		} else if (ball.xpos >= player2.xpos - player2.width && ball.ypos <= player2.ypos + player2.height && ball.ypos >= player2.ypos)
+		{
+			ball.xvel = -5;
+		}
+
+		if (ball.xpos < 0 || ball.xpos > winWidth)
+		{
+			break;
+		}
+			
+		ball.ypos += ball.yvel;
+		ball.xpos += ball.xvel;
+		
+		SDL_Delay(15);
+		
 	}
 
 	SDL_DestroyWindow(win);
